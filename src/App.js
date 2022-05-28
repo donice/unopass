183fd8afd03bf7c3365e8272977a7b590cb986e9
Donice Ubaru <doniceubaru1@gmail.com>
2022-05-28 21:18:10 +0100

    Updated the Hero page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Hero from './components/Hero//Hero.js'
-import Registration from './components/Registration/Registration'
+// import Registration from './components/Registration/Registration'
 import SignIn from './components/Sign-in/Sign-in';
 
 function App() {
   return (
     <div>
       <Hero />
-      {/* <Registration /> */}
+  
       <SignIn />
     </div>
   );
