1481ef8a561541d5f72b75587479a85a5735ea69
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-01 19:13:01 +0100

    updated the reigistration component

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,9 +1,26 @@
 import './Registration.css'
 import Logo from '../../static/images/unopass-blue-logo.png'
 import Favicon from '../../static/images/favicon.png'
-// import Navbar from '../Navbar/Navbar'
+import { useState } from 'react'
 
 const Registration = () => {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: ""
+    })
+
+    console.log(formData)
+
+    const handleChange = (event) => {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
     return (
         <section>
             
@@ -21,24 +38,81 @@ const Registration = () => {
                 </div>
                 <div className='Registration--form'>
                     <nav className='Registration--navbar' >
-                    <img src={Favicon} alt='favicon' className='Registration-navbar--favicon' />
-                        <img src={Logo} alt='logo' className='Registration--navbar--logo' />
+                    <img 
+                        src={Favicon} 
+                        alt='favicon' 
+                        className='Registration-navbar--favicon'
+                    />
+                        <img 
+                            src={Logo} 
+                            alt='logo' 
+                            className='Registration--navbar--logo'
+                        />
+
                         <div>
-                            <button className='Registration--btn'>Sign in</button>
+                            <button 
+                                className='Registration--btn'
+                            >Sign in
+                            </button>
                         </div>
                     </nav>
-                    <form action="#" className='Registration--form-area' >
-                            <h1>Create an account</h1>
-                            <label for="fname">Name</label><br />
-                            <input type="text" id="fname" name="fname" placeholder="" /><br/><br />
-                            <label for="lname">Email Address</label><br />
-                            <input type="text" id="lname" name="lname" placeholder="" /><br/><br/>
-                            <label for="lname">Password</label><br />
-                            <input type="text" id="lname" name="lname" placeholder="" /><br/><br/>
-                            <label for="lname">Confirm Password</label><br />
-                            <input type="text" id="lname" name="lname" placeholder="" /><br/><br/>
-                            <input type="submit" value="Create Account" className='submit' />
-                            <p>By proceeding, you agree to the <a href='#'>Terms of Services</a> and <a href='#'>Privacy Notice</a> </p>
+                    <form 
+                        action="#" 
+                        className='Registration--form-area'
+                        >
+
+                        <h1>Create an account</h1>
+                        <label for="firstName">Name</label>
+                        <br />
+                        <input 
+                            type="text" 
+                            id="firstName" 
+                            name="firstName" 
+                            onChange={handleChange}
+                            placeholder="First Name" 
+                        />
+                        <br/>
+                        <br />
+                        <label for="lastName">Email Address</label>
+                        <br />
+                        <input 
+                            type="text" 
+                            id="lastName" 
+                            name="lastName" 
+                            onChange={handleChange}
+                            placeholder="Last Name" 
+                        />
+                        <br/>
+                        <br/>
+                        <label for="password">Password</label>
+                        <br />
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            placeholder="Password" 
+                        />
+                        <br/>
+                        <br/>
+                        <label for="passwor">Confirm Password</label>
+                        <br />
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            placeholder="Password" 
+                        />
+                        <br/>
+                        <br/>
+                        <input 
+                            type="submit" 
+                            value="Create Account" 
+                            className='submit' />
+                        <p>
+                            By proceeding, you agree to the 
+                            <a href='#'>Terms of Services</a> and 
+                            <a href='#'>Privacy Notice</a> 
+                        </p>
                     </form> 
                 </div>
             </div>
