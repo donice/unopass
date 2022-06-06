200035764ef5abf5319f8cac0262e8d5f39cf8ab
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-06 04:38:47 +0100

    edited the dashboard sidebar

--- src/components/Dashboard/AddItems.js
+++ src/components/Dashboard/AddItems.js
@@ -3,11 +3,50 @@ import React from 'react'
 import Main from './AddItems/Main'
 import Sidebar from './AddItems/Sidebar'
 import { useEffect, useState } from 'react'
+import uuid from 'react-uuid'
 
 export default function AddItems () {
+    const [addItems, setAddItems] = useState([])
+    const [activeItem, setActiveItems] = useState(false)
+
+
+    // Function to add new Item
+    const onAddItem = () => {
+        const newItem = {
+            id: uuid(),
+            title: "New Item",
+            body: "",
+            lastModified: Date.now()
+        }
+        setAddItems([newItem, ...addItems])
+    }
+
+    const updateAddItems = (updateAddItems) => {
+        const updateAddItemsArray = addItems.map((items) => {
+            if(items.id === activeItem) {
+                return updateAddItems;
+            }
+            return items;
+        })
+
+        setAddItems(updateAddItemsArray)
+    }
+
+    // This function deletes Itemsfrom the sidebar
+    const deleteItems = () => {
+        return addItems.find((items) => items.id === activeItem)
+    }
+
+
     return (
         <section className='AddItems'>
-            <Sidebar />
+            <Sidebar 
+                addItems={addItems}
+                onAddItem={onAddItem}
+                deleteItems={deleteItems}
+                activeItem={activeItem}
+                setActiveItems={setActiveItems}
+            />
             <Main /> 
         </section>
     )
--- src/components/Dashboard/AddItems/Sidebar.js
+++ src/components/Dashboard/AddItems/Sidebar.js
@@ -1,6 +1,43 @@
-const Sidebar = () => {
+const Sidebar = (
+    {
+        addItems,
+        onAddItem,
+        deleteItems,
+        activeItem,
+        setActiveItem,
+    }
+) => {
+
+    
+
     return (
-        <div>Sidebar</div>
+        <div className="AddItems-sidebar">
+            <div className="AddItems-sidebar-header">
+                <h1>Welcome Back</h1>
+                <button onClick={onAddItem}>Add</button>
+            </div>
+
+            <div className="AddItems-sidebar-notes">
+                {addItems.map((items) => (
+                    <div
+                        className={`AddItems-sidebar-note ${items.id === activeItem && "active"} `}
+                        onClick={() => setActiveItem(items.id)}
+
+                    >
+                        <div className="AddItems-note-title">
+                            <strong>Title</strong>
+                            <button onClick={() => deleteItems(items.id)}>Delete</button>
+                        </div>
+
+                        <p>Body</p>
+                        <small className="note-meta">
+                            last Modified: Today
+                        </small>
+
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
