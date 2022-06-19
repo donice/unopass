cf87d1c9bab1ec5dec69ff4dd776197477d9d676
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-19 23:33:00 +0100

    updated registration

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,160 +1,271 @@
 import './Registration.css'
 import Logo from '../../static/images/unopass-blue-logo.png'
 import Favicon from '../../static/images/favicon.png'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from '../../api/axios'
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_.]{3,23}$/;
+const EMAIL_REGEX = /^[a-zA-Z][a-zA-Z0-9-_](?=.*[.]).{3,23}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const REGISTER_URL = './create'; // the route on the backend
 
 const Registration = () => {
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        passwordConfirm: ""
-    })
 
-    const handleChange = (event) => {
-        const { name, value, type, checked } = event.target
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [name]: (type === 'checkbox')? checked: value
-            }
-        })
-    }
+	const userRef = useRef();
+   const errRef = useRef();
 
-    const handleSubmit = (event) => {
-        event.preventDefault() // This prevents the form from re-refreshing the form as it is supposed to, by default
-        console.log('Congratulations, You just Registratered')
-    }
+   const [user, setUser] = useState('');
+   const [validUser, setValidUser] = useState(false);
+   const [userFocus, setUserFocus] = useState(false);
+
+   const [email, setEmail] = useState('');
+   const [validEmail, setValidEmail] = useState(false);
+   const [emailFocus, setEmailFocus] = useState(false);
+
+   const [password, setPassword] = useState('');
+   const [validPassword, setValidPassword] = useState(false);
+   const [passwordFocus, setPasswordFocus] = useState(false);
+
+   const [errMssg, setErrMsg] = useState('');
+   const [success, setSucess] = useState(false);
+
+   useEffect(() => {
+      userRef.current.focus();
+   }, [])
+
+   // an effect that tests if the username matches the regex requirements 
+   useEffect(() => {
+      const result = USER_REGEX.test(user);
+      console.log(result);
+      console.log(user)
+      setValidUser(result);
+   }, [user])
+
+   // an effect that tests if the email matches the regex requirements
+   useEffect(() => {
+      const result = EMAIL_REGEX.test(email)
+      console.log(result)
+      console.log(email)
+      setValidEmail(result)
+   }, [email])
+
+   // an effect that checks if the password matches the regex requirements
+   useEffect(() => {
+      const result = PASSWORD_REGEX.test(password);
+      console.log(result);
+      console.log(password)
+      setValidPassword(result);
+   }, [password])
+
+   // an effect that clears the error message when either of the dependencies changes 
+   useEffect(() => {
+      setErrMsg('')
+   }, [user, email, password])
 
 
-    return (
-        <section>
-            
-            <div className='Registration'>
-                <div className='Registration-side-bar'>
-                    <img src={Favicon} alt='favicon' />
-                    <h1>Safe. <br />
-                        Secure. <br />
-                        Swift. <br />
-                    </h1>
-                    <p>You'd never have to worry about the security <br />
-                     of your accounts, ever again. <br />
-                     Start today, with a click
-                    </p>
-                </div>
-                <div className='Registration--form'>
-                    <nav className='Registration--navbar' >
-                    <img 
-                        src={Favicon} 
-                        alt='favicon' 
-                        className='Registration-navbar--favicon'
-                    />
-                        <img 
-                            src={Logo} 
-                            alt='logo' 
-                            className='Registration--navbar--logo'
-                        />
 
-                        <div>
-                            {/* <Link to='/signin'>
-                                <button 
-                                    className='Registration--btn'
-                                >Sign in
-                                </button>
-                            </Link> */}
-                        </div>
-                    </nav>
-                    <form 
-                        onSubmit={handleSubmit} 
-                        className='Registration--form-area'
-                        >
+	//.............................
 
-                        <h1>Create an account</h1>
-                        <label htmlFor="firstName">First Name</label>
-                        <br />
-                        <input 
-                            type="text" 
-                            id="firstName" 
-                            name="firstName" 
-                            onChange={handleChange}
-                            placeholder="First Name"
-                            value={formData.firstName} 
-                        />
-                        <br/>
-                        <br />
-                        
-                        <label htmlFor="lastName">Last Name</label>
-                        <br />
-                        <input 
-                            type="text" 
-                            id="lastName" 
-                            name="lastName" 
-                            onChange={handleChange}
-                            placeholder="Last Name" 
-                            value={formData.lastName}
-                        />
-                        <br/>
-                        <br/>
+	// handleSubmit function
+   const handleSubmit = async (e) => {
+      e.preventDefault();
 
-                        <label htmlFor="email">Email</label>
-                        <br />
-                        <input 
-                            type="email" 
-                            id="email" 
-                            name="email" 
-                            onChange={handleChange}
-                            placeholder="Email" 
-                            value={formData.email}/>
-                        <br/>
-                        <br/>
+      const v1 = USER_REGEX.test(user)
+      const v2 = EMAIL_REGEX.test(email)
+      const v3 = PASSWORD_REGEX.test(password)
+      if (!v1 || !v2 || !v3) {
+         setErrMsg('inavlid Entry');
+         return;
+      }
+      // .......................................
 
-                        <label htmlFor="password">Password</label>
-                        <br />
-                        <input 
-                            type="password" 
-                            id="password" 
-                            name="password" 
-                            onChange={handleChange}
-                            placeholder="Password" 
-                            value={formData.password}/>
+      try{
+         fetch(` axios ${REGISTER_URL} `, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              name: user,           
+              email,
+              password,
+            })
+         }).then(function(response) {
+            return response.json();
+         }).then(setErrMsg('No server Response'))
+      }
 
-                        <br/>
-                        <br/>
-                        <label htmlFor="passwor">Confirm Password</label>
-                        <br />
-                        <input 
-                            type="password" 
-                            id="passwordConfirm" 
-                            name="passwordConfirm" 
-                            onChange={handleChange}
-                            placeholder="Password" 
-                            value={formData.passwordConfirm}/>
-                        <br/>
-                        <br/>
-                        <Link to='/signin'>
-                            <input 
-                                type="submit" 
-                                value="Create Account" 
-                                className='submit' 
-                            />
-                        </Link><br />
-                        <span>
-                            <a href='/' >
-                                Already have an account?
-                            </a>
-                        </span>
-                        <p>
-                            By proceeding, you agree to the 
-                            <a href='/'>Terms of Services</a> and 
-                            <a href='/'>Privacy Notice</a> 
-                        </p>
-                    </form> 
-                </div>
+      catch(err) {
+         if(err) {
+            console.log(err)
+         }
+         if (!err?.response) {
+            setErrMsg('No server Response')
+         } else if (err.response?.status === 402) {
+            setErrMsg('Username Taken')
+         } else {
+            setErrMsg('Registration Failed')
+         }
+         errRef.current.focus()
+      }
+   }
+
+
+   return (
+      <section>      
+         <div className='Registration'>
+            <div className='Registration-side-bar'>
+               <img src={Favicon} alt='favicon' />
+               <h1>Safe. <br />
+                	Secure. <br />
+                	Swift. <br />
+               </h1>
+               <p>You'd never have to worry about the security <br />
+                	of your accounts, ever again. <br />
+                	Start today, with a click
+               </p>
             </div>
-        </section>
-    )
+            <div className='Registration--form'>
+               <nav className='Registration--navbar' >
+                	<img 
+                	   src={Favicon} 
+                	   alt='favicon' 
+                	   className='Registration-navbar--favicon'
+                	/>
+                	<img 
+                	   src={Logo} 
+                	   alt='logo' 
+                	   className='Registration--navbar--logo'
+                	/>
+               </nav>
+
+               <form 
+                	onSubmit={handleSubmit} 
+                	className='Registration--form-area'
+                	>
+
+                	<h1>Create an account</h1>
+
+						<label htmlFor="username">
+                     Username:
+                     <span className={validUser ? 'valid' : 'hide'} >
+                           <FontAwesomeIcon icon={faCheck} />
+                     </span>
+                     <span className={validUser || !user ? 'hide' : 'invalid'} >
+                           <FontAwesomeIcon icon={faTimes} />
+                     </span>
+                  </label>
+						<br />
+                  <input 
+                     type = 'text'
+                     id = "username" //matches the label's htmlFor
+                     ref = {userRef}
+                     autoComplete = 'off'
+                     onChange = {(e)  => setUser(e.target.value)}
+                     required
+                     aria-invalid = {validUser ? 'false' : 'true'}
+                     aria-describedby = 'uidnote' // matches the error paragraph id
+                     onFocus = {() => setUserFocus(true)}
+                     onBlur = {() => setUserFocus(false)}
+                  />
+                  <p id="uidnote" className={userFocus && user && !validUser 
+                  ? "instructions" : "offscreen" } >
+                     <FontAwesomeIcon icon={faInfoCircle} />
+                     4 to 24 characters. <br />
+                     Must begin with a letter. <br />
+                     Letters, numbers, underscores hyphens allowed.
+                  </p>
+                	<br/>
+                	<br />
+		
+                  <label htmlFor="email">
+                     Email:
+                     <span className={validEmail ? 'valid' : 'hide'} >
+                           <FontAwesomeIcon icon={faCheck} />
+                     </span>
+                     <span className={validEmail || !email ? 'hide' : 'invalid'} >
+                           <FontAwesomeIcon icon={faTimes} />
+                     </span>
+                  </label><br />
+                  <input 
+                     type = 'text'
+                     id = "email"  // matches the label's htmlFor
+                     onChange = {(e)  => setEmail(e.target.value)}
+                     required
+                     aria-invalid = {validEmail ? 'false' : 'true'}
+                     aria-describedby = 'emailconfirm' // matches the error paragraph id
+                     onFocus = {() => setEmailFocus(true)}
+                     onBlur = {() => setEmailFocus(false)}
+                  />
+                  <p id="emailconfirm" className={emailFocus && !validEmail ? "instructions" : "offscreen" } >
+                     <FontAwesomeIcon icon={faInfoCircle} />
+                     Enter a valid email address
+                  </p>
+
+                	<br/>
+                	<br/>
+
+						<label htmlFor="password">
+                     Password:
+                     <span className={validPassword ? 'valid' : 'hide'} >
+                           <FontAwesomeIcon icon={faCheck} />
+                     </span>
+                     <span className={validPassword || !password ? 'hide' : 'invalid'} >
+                           <FontAwesomeIcon icon={faTimes} />
+                     </span>
+                  </label><br />
+                  <input 
+                     type = 'password'
+                     id = "password"  // matches the label's htmlFor
+                     onChange = {(e)  => setPassword(e.target.value)}
+                     autoComplete= 'false'
+                     required
+                     aria-invalid = {validPassword ? 'false' : 'true'}
+                     aria-describedby = 'pwdnote' // matches the error paragraph id
+                     onFocus = {() => setPasswordFocus(true)}
+                     onBlur = {() => setPasswordFocus(false)}
+                  />
+                  <p id="pwdnote" className={passwordFocus && !validPassword ? "instructions" : "offscreen" } >
+                     <FontAwesomeIcon icon={faInfoCircle} />
+                     4 to 24 characters. <br />
+                     Must include uppercase and lowercase letters, a number and a special character <br />
+                     Allowed special characters: 
+                     <span aria-label="exclamation mark">!</span>
+                     <span aria-label="at symbol">@</span>
+                     <span aria-label="hashtag">#</span>
+                     <span aria-label="dollar sign">$</span>
+                     <span aria-label="percent">%</span>
+                  </p>
+                	<br/>
+                	<br/>
+
+                	<Link to='/signin'>
+                	   <input 
+                	     type="submit" 
+                	     value="Create Account" 
+                	     className='submit' 
+                	   />
+                	</Link><br />
+                	<span>
+                	   <a href='/' >
+                	     Already have an account?
+                	   </a>
+                	</span>
+                	<p>
+                	   By proceeding, you agree to the 
+                	   <a href='/'>Terms of Services</a> and 
+                	   <a href='/'>Privacy Notice</a> 
+                	</p>
+                	  </form> 
+               </div>
+            </div>
+      </section>
+   )
 }
 
 export default Registration;
