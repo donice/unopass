8a0375c54d3a9e941346b4be85d62cc5a863d738
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-20 00:45:31 +0100

    updated registration

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -11,7 +11,7 @@ const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_.]{3,23}$/;
 const EMAIL_REGEX = /^[a-zA-Z][a-zA-Z0-9-_](?=.*[.]).{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-const REGISTER_URL = './create'; // the route on the backend
+const REGISTER_URL = './create'; // the route on the backend url
 
 const Registration = () => {
 
